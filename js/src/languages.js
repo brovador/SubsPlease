1f6d0a102aa4dccc2c993e14f50d46cadfc574bf
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2015-10-13 22:30:22 +0200

    First commit

---
+++ js/src/languages.js
@@ -0,0 +1,10 @@
+var languages = {
+  'order' : ['es', 'en', 'it'],
+  'info' : {
+    'es' : {'name' : 'Spanish', 'code' : 'spa'},
+    'en' : {'name' : 'English', 'code' : 'eng'},
+    'it' : {'name' : 'Italian', 'code' : 'ita'}
+  }
+}
+
+module.exports = languages
