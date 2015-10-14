80d3bc4ec1192eaf14039f7e830a61eb6a86fe0b
Jesús Bartús Sancho <jesus.bartus@gmail.com>
2015-10-14 22:54:26 +0200

    updated languages

--- js/dst/languages.js
+++ js/dst/languages.js
@@ -5,7 +5,9 @@ var languages = {
   'info': {
     'es': { 'name': 'Spanish', 'code': 'spa' },
     'en': { 'name': 'English', 'code': 'eng' },
-    'it': { 'name': 'Italian', 'code': 'ita' }
+    'it': { 'name': 'Italian', 'code': 'ita' },
+    'fr': { 'name': 'French', 'code': 'fre' },
+    'pt': { 'name': 'Portuguese', 'code': 'por' }
   }
 };
 
